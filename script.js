9dc5958e0416ffb75e5e01ab67ff2afafff28a44
unknown <bbb155072@gmail.com>
2025-06-18 18:06:32 +0300

    Создание главной страницы

---
+++ script.js
@@ -0,0 +1,14 @@
+const right_btn = document.querySelector('.right__1')
+const left_btn = document.querySelector('.left__1')
+
+const reviewsBlock = document.querySelector('.reviews__1')
+
+left_btn.addEventListener('click', () => {
+   reviewsBlock.scrollBy(-1000, 0)
+})
+
+right_btn.addEventListener('click', () => {
+   reviewsBlock.scrollBy(1000, 0)
+})
+
+
